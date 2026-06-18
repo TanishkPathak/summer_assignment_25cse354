5fadabf445de60666522b4b8ec088ff37e786a4d
TanishkPathak <Tanishkpathak2008@gmail.com>
2026-06-18 19:44:11 +0530

    Added day03 assignments

---
+++ day03/Q4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, max, lcm;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    // Find the greater number
+    max = (num1 > num2) ? num1 : num2;
+
+    while (1)
+    {
+        if (max % num1 == 0 && max % num2 == 0)
+        {
+            lcm = max;
+            break;
+        }
+        max++;
+    }
+
+    printf("LCM of %d and %d = %d", num1, num2, lcm);
+
+    return 0;
+}
