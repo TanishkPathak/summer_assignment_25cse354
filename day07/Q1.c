f6ab53b285162e980aec00cff1056f5b01b8ee14
TanishkPathak <Tanishkpathak2008@gmail.com>
2026-06-19 11:09:14 +0530

    Added day07 assignments

---
+++ day07/Q1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Function to calculate factorial recursively
+int factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Factorial of %d = %d", n, factorial(n));
+
+    return 0;
+}
